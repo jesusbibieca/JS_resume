c00836ca0a52e39bb5d1e517678d7ab8bf1d37ce
Jesus Bibieca <jesusbibieca52@gmail.com>
2017-12-03 01:05:16 -0400

    add bio to js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,5 +7,19 @@ var role = 'Full Stack Developer';
 var formattedRole = HTMLheaderRole.replace('%data%', role);
 $('#header').append(formattedRole);
 
-var skills = ['Awesomeness', 'Programming', 'Python', 'JS'];
-$('#main').append(skills);
+var skills = ['Programming', 'Python', 'JS', 'CSS', 'Fast Learner', 'Passionate'];
+// $('#main').append(skills.length);
+
+var bio = {
+	'name': 'Jesus',
+	'age': 21,
+	'skills': skills,
+	'role': 'Full Stack Developer',
+	'tel': '809-222-2219',
+	'cel': '829-939-4555',
+	'github': 'https://github.com/jesusbibieca',
+	'mail': 'jesusbibieca52@gmail.com',
+	'picture': 'https://lh3.googleusercontent.com/-jvfcuYhGlVE/Wfi_ObBNf_I/AAAAAAAAAs0/aJG7Bu4S-8wcBppn7wGEUx3i-a3BtkiqACEwYBhgL/w139-h140-p/Profile.PNG',
+	'message': 'This is the welcome msg for now.'
+};
+$('#main').append(bio.name);
